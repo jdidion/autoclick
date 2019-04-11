a2b548b93c2ddb71d9e7d422c69a63651b09c921
jdidion <github@didion.net>
2019-04-11 09:52:47 -0400

    Expose set_global

--- autoclick/__init__.py
+++ autoclick/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 
-from autoclick.utils import LOG
+from autoclick.utils import LOG, set_global
 from autoclick.commands import command, group
 from autoclick.core import SignatureError, ParameterCollisionError
 from autoclick.composites import (
